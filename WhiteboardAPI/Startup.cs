89a5388c2413c0444d34f18010b8f34dfc1903e4
yuxuan <yuxuan9697@gmail.com>
2020-03-07 22:04:34 +0800

    expose x-pagination in response header

--- WhiteboardAPI/Startup.cs
+++ WhiteboardAPI/Startup.cs
@@ -148,7 +148,7 @@ namespace WhiteboardAPI
             app.UseCors(x => x
                 .AllowAnyOrigin()
                 .AllowAnyMethod()
-                .AllowAnyHeader());
+                .WithExposedHeaders("content-encoding", "content-length", "conten-type", "date", "server", "x-pagination"));
 
             app.UseEndpoints(endpoints =>
             {
