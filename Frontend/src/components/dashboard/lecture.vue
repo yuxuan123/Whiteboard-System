4d194dcec6f7e4ad5d22fc338d2ace5acf37616f
Jiawei Tan <JiaweiTan1996@gmail.com>
2020-03-15 01:18:00 +0800

    Remove lecture video functions

--- Frontend/src/components/dashboard/lecture.vue
+++ Frontend/src/components/dashboard/lecture.vue
@@ -129,17 +129,7 @@
                 class="vjs-custom-skin"
                 :options="playerOptions"
                 :playsinline="true"
-                @play="onPlayerPlay($event)"
-                @pause="onPlayerPause($event)"
-                @ended="onPlayerEnded($event)"
-                @loadeddata="onPlayerLoadeddata($event)"
-                @waiting="onPlayerWaiting($event)"
-                @playing="onPlayerPlaying($event)"
-                @timeupdate="onPlayerTimeupdate($event)"
-                @canplay="onPlayerCanplay($event)"
-                @canplaythrough="onPlayerCanplaythrough($event)"
-                @ready="playerReadied"
-                @statechanged="playerStateChanged($event)"
+                 @ready="playerReadied"
               />
             </v-layout>
           </v-card-text>
@@ -241,38 +231,6 @@ export default {
     };
   },
   methods: {
-    // listen event
-    onPlayerPlay(player) {
-      // console.log('player play!', player)
-    },
-    onPlayerPause(player) {
-      // console.log('player pause!', player)
-    },
-    onPlayerEnded(player) {
-      // console.log('player ended!', player)
-    },
-    onPlayerLoadeddata(player) {
-      // console.log('player Loadeddata!', player)
-    },
-    onPlayerWaiting(player) {
-      // console.log('player Waiting!', player)
-    },
-    onPlayerPlaying(player) {
-      // console.log('player Playing!', player)
-    },
-    onPlayerTimeupdate(player) {
-      // console.log('player Timeupdate!', player.currentTime())
-    },
-    onPlayerCanplay(player) {
-      // console.log('player Canplay!', player)
-    },
-    onPlayerCanplaythrough(player) {
-      // console.log('player Canplaythrough!', player)
-    },
-    // or listen state event
-    playerStateChanged(playerCurrentState) {
-      // console.log('player current update state', playerCurrentState)
-    },
     // player is ready
     playerReadied(player) {
       player.currentTime(0);
