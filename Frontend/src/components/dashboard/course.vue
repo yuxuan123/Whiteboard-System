2162de4f3c829de3b1da344a9e5b4b325c1c5308
Jiawei Tan <JiaweiTan1996@gmail.com>
2020-03-16 01:12:38 +0800

    Update Course.vue

--- Frontend/src/components/dashboard/course.vue
+++ Frontend/src/components/dashboard/course.vue
@@ -1,46 +1,74 @@
 <template>
-  <v-container fluid grid-list-xl fill-height>
+  <v-container
+    fluid
+    grid-list-xl
+    fill-height
+  >
     <v-layout justify-center>
       <v-flex xs12>
         <material-card color="general">
           <div slot="header">
-            <div class="title font-weight-light mb-2">Courses</div>
+            <div class="title font-weight-light mb-2">
+              Courses
+            </div>
           </div>
-          <v-flex md12 sm12>
+          <v-flex
+            md12
+            sm12
+          >
             <div>
-              <v-dialog v-model="dialog" width="600px">
+              <v-dialog
+                v-model="dialog"
+                width="600px"
+              >
                 <template v-slot:activator="{ on }">
                   <v-btn
+                    v-if="$cookies.get('userid') != 'student'"
                     rounded
                     dark
                     class="general"
-                    v-if="!userRole === 'student'"
                     v-on="on"
-                    >Add Course</v-btn
                   >
+                    Add Course
+                  </v-btn>
                 </template>
                 <v-card>
                   <v-card-title>
                     <span class="headline">Create Course</span>
                   </v-card-title>
                   <v-card-text>
-                    <v-container class="pt-0" grid-list-md>
+                    <v-container
+                      class="pt-0"
+                      grid-list-md
+                    >
                       <v-layout wrap>
-                        <v-flex xs12 sm6 md6>
+                        <v-flex
+                          xs12
+                          sm6
+                          md6
+                        >
                           <v-text-field
                             v-model="createContent.CourseCode"
                             label="Course Code"
                           />
                         </v-flex>
 
-                        <v-flex xs12 sm6 md6>
+                        <v-flex
+                          xs12
+                          sm6
+                          md6
+                        >
                           <v-text-field
                             v-model="createContent.CourseName"
                             label="Course Name"
                           />
                         </v-flex>
 
-                        <v-flex xs12 sm12 md12>
+                        <v-flex
+                          xs12
+                          sm12
+                          md12
+                        >
                           <v-text-field
                             v-model="createContent.CourseDescription"
                             label="Course Description"
@@ -50,27 +78,41 @@
                     </v-container>
                   </v-card-text>
                   <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="green darken-1" flat="flat" @click="addCourse"
-                      >Create</v-btn
+                    <v-spacer />
+                    <v-btn
+                      color="green darken-1"
+                      flat="flat"
+                      @click="addCourse"
                     >
+                      Create
+                    </v-btn>
                   </v-card-actions>
                 </v-card>
               </v-dialog>
 
               <vue-tree-list
-                @click="onClick"
-                @change-name="onChangeName"
-                @delete-node="onDel"
-                @add-node="onAddNode"
+                class="mt-3"
                 :model="courseCodeTree"
                 default-tree-node-name="new Course"
                 default-leaf-node-name="new Content"
-                v-bind:default-expanded="false"
+                :default-expanded="false"
+                @click="onClick"
+                @change-name="onChangeName"
+                @delete-node="deleteCourseCodeNode"
+                @add-node="onAddNode"
               >
-                <span class="icon" slot="leafNodeIcon">📃</span>
-                <span class="icon" slot="treeNodeIcon">📂</span>
-                <span class="icon" slot>🌲</span>
+                <span
+                  slot="leafNodeIcon"
+                  class="icon"
+                >📃</span>
+                <span
+                  slot="treeNodeIcon"
+                  class="icon"
+                >📂</span>
+                <span
+                  slot
+                  class="icon"
+                >🌲</span>
               </vue-tree-list>
             </div>
           </v-flex>
@@ -104,11 +146,7 @@ export default {
     //Check if the user role is a student or professor
     //Get User courses or All Course
     //Add the course to tree
-    if (this.userRole === "student") {
-      this.getUserCourses();
-    } else {
-      this.getAllCourses();
-    }
+    this.getUserCourses();
   },
   methods: {
     getUserCourses() {
@@ -117,18 +155,14 @@ export default {
         this.getCourseContent(this.userId);
       });
     },
-    getAllCourses() {
-      this.$store.dispatch("GETALLCOURSES").then(response => {
-        this.courses = response.data;
-        this.getCourseContent(this.userId);
-      });
-    },
+
     getCourseContent(userId) {
       this.$store.dispatch("GETUSERCONTENT", userId).then(response => {
         this.courseContents = response.data;
         this.addTreeNodeChildren();
       });
     },
+
     addTreeNodeChildren() {
       var node = {};
       for (var i = 0; i < this.courses.length; i++) {
@@ -156,6 +190,7 @@ export default {
         this.addTreeNodeLeaf(this.courses[i].courseId, i);
       }
     },
+
     addTreeNodeLeaf(courseId, index) {
       for (var i = 0; i < this.courseContents.length; i++) {
         if (
@@ -189,63 +224,11 @@ export default {
         }
       }
     },
-    onDel(node) {
-      if (
-        (node.name.charAt(0) === "C" || node.name.charAt(0) === "c") &&
-        (node.name.charAt(1) === "Z" || node.name.charAt(1) === "z")
-      ) {
-        this.axios
-          .delete(
-            "https://whiteboardsyetem.azurewebsites.net/deleteCourse/" + node.id
-          )
-          .then(function(response) {});
-      } else {
-        this.axios
-          .delete(
-            "https://whiteboardsyetem.azurewebsites.net/deleteContent/" +
-              node.id
-          )
-          .then(function(response) {});
-      }
-      node.remove();
-    },
 
-    onChangeName(params) {
-      var myObj = {
-        contentId: params.id,
-        courseId: params.id,
-        type: "null",
-        title: params.newName,
-        description: "null",
-        datetime: "0001-01-01T00:00:00Z",
-        fileName: params.newName + ".doc",
-        url: params.newName + ".doc",
-        createdOn: "0001-01-01T00:00:00Z",
-        createdBy: "d99dfc08-5d7a-4e04-c824-08d7c5959f4d"
-      };
-      this.axios
-        .put("https://whiteboardsyetem.azurewebsites.net/updateContent", myObj)
-        .then(function(response) {});
-      console.log(params);
-    },
-
-    onAddNode(params) {
-      console.log(params.name);
-    },
-
-    addNode() {
-      var node = new TreeNode({ name: "new node", isLeaf: false });
-      if (!this.data.children) this.data.children = [];
-      this.data.addChildren(node);
-    },
     addCourse() {
       let cur = this;
-      let role = $cookies.get("role");
-      let userId = $cookies.get("userid");
-      console.log(this.createContent);
-      this.createContent.CreatedBy = "70e2777c-401a-4769-4721-08d7c2644faa";
-
-      if (role === "student") {
+      this.createContent.CreatedBy = this.userId;
+      if (this.userRole === "lecturer") {
         this.axios
           .post(
             "https://whiteboardsyetem.azurewebsites.net/createCourse?" +
@@ -259,9 +242,9 @@ export default {
               this.createContent.CreatedBy
           )
           .then(function(response) {
-            console.log(response);
             var node = new TreeNode({
-              name: response.data.courseName,
+              id: response.data.courseId,
+              name: response.data.courseCode + " - " + response.data.courseName,
               isLeaf: false,
               dragDisabled: true,
               addLeafNodeDisabled: true,
@@ -271,31 +254,82 @@ export default {
             cur.courseCodeTree.addChildren(node);
             cur.dialog = false;
             cur.createContent = {};
+            cur.regLecturerToCourse(
+              response.data.courseId,
+              response.data.createdBy
+            );
           });
       }
     },
-    onClick(params) {
-      let userId = $cookies.get("userid");
-      var role;
 
-      this.axios
-        .get(
-          "https://whiteboardsyetem.azurewebsites.net/getUsers?userIds=" +
-            userId
-        )
-        .then(function(response) {
-          role = response.data.userDtos[0].role;
-          if (
-            params.name.charAt(0) !== "C" &&
-            params.name.charAt(0) !== "c" &&
-            params.name.charAt(0) !== "Z" &&
-            params.name.charAt() !== "z" &&
-            role === "student"
-          ) {
-            confirm("Do you want to download this?");
-          }
-        });
+    regLecturerToCourse(courseId, createdBy) {
+      var item = {
+        courseId: courseId,
+        staffId: createdBy,
+        isActive: true
+      };
+      this.$store.dispatch("ADDCOURSESTAFF", item);
+    },
+
+    deleteCourseCodeNode(node) {
+      var answer = window.confirm("Confirm delete?");
+      if (answer) {
+        if (
+          (node.name.charAt(0) === "C" || node.name.charAt(0) === "c") &&
+          (node.name.charAt(1) === "Z" || node.name.charAt(1) === "z")
+        ) {
+          this.$store.dispatch("DELETECOURSE", node.id);
+        } else {
+          this.$store.dispatch("DELETECONTENT", node.id);
+        }
+        node.remove();
+      }
+    },
+
+    onAddNode(params) {
+      console.log(params.name);
+    },
+
+    addNode() {
+      var node = new TreeNode({ name: "new node", isLeaf: false });
+      if (!this.data.children) this.data.children = [];
+      this.data.addChildren(node);
+    },
+
+    onChangeName(params) {
+      console.log(params);
+    },
+
+    onClick(params) {
+      if (
+        params.name.charAt(0) !== "C" &&
+        params.name.charAt(0) !== "c" &&
+        params.name.charAt(0) !== "Z" &&
+        params.name.charAt() !== "z" &&
+        this.userRole === "student"
+      ) {
+        confirm("Do you want to download this?");
+      }
     }
   }
 };
 </script>
+<style>
+.vtl-node {
+  padding: 1px !important;
+  border-radius: 10px !important;
+  border-color: #448aff;
+  border-style: solid;
+  border-width: 0.5px;
+  margin-top: 10px !important;
+  margin-bottom: 20px !important;
+}
+
+.vtl-node-main:hover {
+  border-radius: 10px !important;
+}
+.vtl-node-content {
+  padding: 10px !important;
+  font-size: 18px !important;
+}
+</style>
